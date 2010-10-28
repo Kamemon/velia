ac6d54315aab98a431ca91a2ccc28651788860ee
leonardofl87 <leonardofl87@94cb3acf-4556-2c03-1130-da5e810666b3>
2010-10-28 23:05:08 +0000

    Coisas processing

--- velia/src/usp/ia/velia/graph/Processing.java
+++ velia/src/usp/ia/velia/graph/Processing.java
@@ -1,5 +1,22 @@
 package usp.ia.velia.graph;
 
-public class Processing {
+import processing.core.PApplet;
+import usp.ia.velia.Jogo;
 
+@SuppressWarnings("serial")
+public class Processing extends PApplet {
+
+    private Jogo jogo;
+    private Tabuleiro tab;
+    
+    public void setup() {
+
+        this.jogo = new Jogo();
+        this.tab = new Tabuleiro(jogo, this);
+    }
+    
+    public void draw() {
+        
+        this.tab.draw();
+    }
 }
