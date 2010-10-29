9e62ecf8495e05a685a22a56164ea4777ef50d05
leonardofl87 <leonardofl87@94cb3acf-4556-2c03-1130-da5e810666b3>
2010-10-29 02:11:40 +0000

    Graph monalisa!

--- velia/src/usp/ia/velia/graph/Tabuleiro.java
+++ velia/src/usp/ia/velia/graph/Tabuleiro.java
@@ -10,12 +10,12 @@ public class Tabuleiro {
     private final int N = 3;
     
     private Jogo jogo;
-    private PApplet processing;
+    private PApplet sketch;
     
     
     public Tabuleiro(PApplet processing, Jogo jogo) {
         this.jogo = jogo;
-        this.processing = processing;
+        this.sketch = processing;
     }
 
     
@@ -27,19 +27,44 @@ public class Tabuleiro {
         Jogador[][][] tab = this.jogo.viewTabuleiro(); 
         
         int e = 10; // espaçamento
+        
+        // desenha divisões (linhas)
+        this.sketch.stroke(255, 0, 0);
+        this.sketch.line(-10, 5, 5, 30, 5, 5);
+        this.sketch.line(-10, 5, 15, 30, 5, 15);
+        this.sketch.line(-10, 5, 25, 30, 5, 25);
+
+        this.sketch.line(-10, 15, 5, 30, 15, 5);
+        this.sketch.line(-10, 15, 15, 30, 15, 15);
+        this.sketch.line(-10, 15, 25, 30, 15, 25);
+
+        this.sketch.line(5, -10, 5, 5, 30, 5);
+        this.sketch.line(5, -10, 15, 5, 30, 15);
+        this.sketch.line(5, -10, 25, 5, 30, 25);
+
+        this.sketch.line(15, -10, 5, 15, 30, 5);
+        this.sketch.line(15, -10, 15, 15, 30, 15);
+        this.sketch.line(15, -10, 25, 15, 30, 25);
+
+        
+
+        // desenha insígnias
+        this.sketch.stroke(0, 0, 0);
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
                 for (int k=0; k<N; k++) {
                     
-                    this.processing.translate(i*e, j*e, k*e);
+                    this.sketch.translate(i*e, j*e, k*e); // posicionamento da insígnia
                     
                     if (tab[i][j][k] != null) { // posição não vazia
                         Jogador jog = tab[i][j][k]; 
                         if (jog.getInsignia() == Insignia.X)
-                            this.processing.box(3);
+                            this.sketch.box(3);
                         if (jog.getInsignia() == Insignia.O)
-                            this.processing.sphere(3);
-                    }
+                            this.sketch.sphere(2);
+                    } 
+                    
+                    this.sketch.translate(-i*e, -j*e, -k*e);
                 }
             }
         }
