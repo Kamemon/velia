6520ece8cf0f2f5d9d7ce5b3df42459165a8725b
ixacaba <ixacaba@94cb3acf-4556-2c03-1130-da5e810666b3>
2010-11-25 11:33:01 +0000

    arrumado Jogo.getRisca, novos metodos em Posicao

--- velia/src/usp/ia/velia/Posicao.java
+++ velia/src/usp/ia/velia/Posicao.java
@@ -15,6 +15,13 @@ public class Posicao {
         this.coord[2] = z;
     }
 
+    static public Posicao moddif(Posicao a,Posicao b){
+    	//System.out.println(a.coord[0]);
+    	//System.out.println(b.coord[0]);
+    	
+    	return new Posicao( Math.abs(a.coord[0]-b.coord[0]),Math.abs(a.coord[1]-b.coord[1]),Math.abs(a.coord[2]-b.coord[2]));
+    }
+    
     public Posicao() {
 
     }
@@ -30,6 +37,33 @@ public class Posicao {
         int z = coord[2];
         return "("+x+","+y+","+z+")";
     }
+    
+    @Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(coord);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicao other = (Posicao) obj;
+		if (!Arrays.equals(coord, other.coord))
+			return false;
+		return true;
+	}
+
+//	public boolean equals(Object to){
+//    	if(coord[0]==to.coord[0] && coord[1]==to.coord[1] && coord[2]==to.coord[2])return true;
+//    	return false;
+//    }
     
     
     
